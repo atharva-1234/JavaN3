7fd519436be2881add1f223786567545c587da66
user <user@DESKTOP-IM10N6S>
2023-02-01 07:07:23 +0530

    First Commit Uploading all the files

---
+++ dayonee/src/org/tnsif/intro/JavaProgramDayone.java
@@ -0,0 +1,10 @@
+package org.tnsif.intro;
+
+public class JavaProgramDayone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.print("HEloo world");
+	}
+
+}
