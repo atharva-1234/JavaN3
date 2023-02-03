855e454dfc60038c043252a524946b07f55e6d2d
user <user@DESKTOP-IM10N6S>
2023-02-03 13:56:58 +0530

    third commit

--- daytwo/src/org/tnsif/operator/OperatorDemo.java
+++ daytwo/src/org/tnsif/operator/OperatorDemo.java
@@ -23,7 +23,7 @@ public class OperatorDemo {
       
     // logical operator 
        boolean res2=(5==5)&&(5>3); //true
-       boolean res3=(5==3) || (5.2); //true 
+       boolean res3=(5==3) || (5>7); //true 
        boolean res4= !(5==4);
        System.out.println(res2);
        System.out.println(res3);
