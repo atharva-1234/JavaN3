e4ab3ad8eba915ab8d5efe55c85d88a3512ddaf4
user <user@DESKTOP-IM10N6S>
2023-02-27 01:28:39 +0530

    First Commit

---
+++ daysixteen/Test/org/tnsif/junit/FristJavaProgramTesting.java
@@ -0,0 +1,24 @@
+package org.tnsif.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+// 
+@SuppressWarnings("unused")
+class FristJavaProgramTesting {
+
+	@Test
+	@DisplayName(" simple junit testing method ")
+	void test() {
+		System.out.println(" First junit test case");
+	}
+	@Test
+	
+	void display() {
+		System.out.println(" welcome to junit5 testing ");
+	}
+
+
+}
---
+++ daysixteen/Test/org/tnsif/junit/ParameterizedTestCaseDemo.java
@@ -0,0 +1,26 @@
+package org.tnsif.junit;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+// program on parameterized test
+class ParameterizedTestCaseDemo {
+
+	//@Test
+	@ParameterizedTest
+	@ValueSource(strings={" nia","diya"})
+	
+	void test() {
+		System.out.println("hello");
+	}
+	@ParameterizedTest
+	@ValueSource(strings={" nia","diya"})
+	
+	void display(String str) {
+		assertNotNull(str);
+	}
+
+}
