75f48bc6b37a75cc0915ab1b33f192a0369f476c
user <user@DESKTOP-IM10N6S>
2023-02-28 06:44:11 +0530

    First Commit

---
+++ dayfourteen/src/org/tnsif/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package org.tnsif.service;
+
+import java.util.List;
+
+import org.tnsif.entity.Employee;
+
+public interface EmployeeService {
+	//abstract method for CRUD operation
+		public boolean addEmployee(Employee employee);
+		public Employee updateEmployee(Employee employee);
+		public boolean deleteEmployee(int userid);
+		public Employee getEmployee(int userid);
+		public List<Employee>getAll();
+
+}
